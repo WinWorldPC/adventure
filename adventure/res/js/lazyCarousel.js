1c64b88fcaf01ae1fed2955346f393150ae8e1d6
Calvin <calvin@openmailbox.org>
2017-09-18 15:34:46 -0300

    put res directory back to the way it was, add some stuff to seperate things out
    Plus add some "core" stylesheets to it

---
+++ adventure/res/js/lazyCarousel.js
@@ -0,0 +1,9 @@
+﻿// Lazy loading for screenshot carousel
+$(function () {
+    return $(".carousel.lazy").on("slide.bs.carousel", function (ev) {
+        var lazy;
+        lazy = $(ev.relatedTarget).find("img[data-src]");
+        lazy.attr("src", lazy.data('src'));
+        lazy.removeAttr("data-src");
+    });
+});
