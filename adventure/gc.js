f4a74a41d7e9049b6564b6b68df4fa7c3f19d36f
Calvin <calvin@openmailbox.org>
2017-09-27 22:46:14 -0300

    Add the GC script to be called by cron or whatever
    Takes config file as param

---
+++ adventure/gc.js
@@ -0,0 +1,17 @@
+﻿// Garbage collection script
+
+var database = require("./database.js");
+
+var config = JSON.parse(fs.readFileSync(process.argv[2], "utf8").replace(/^\uFEFF/, ""));
+
+database.createConnection(config.mysql);
+
+const downloadHitsQuery = "DELETE FROM DownloadHits WHERE DATE_SUB(DownloadTime,INTERVAL 1 DAY) > CURDATE()";
+
+database.execute(downloadHitsQuery, [], function (dhErr, dhRes, dhFields) {
+    if (dhErr) {
+        console.log(dhErr);
+    } else {
+        console.log("Deleted stable download hits");
+    }
+});
