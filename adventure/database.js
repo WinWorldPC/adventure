92fad18764d4fbe6b5883f1f7c8d596569adeccd
Calvin Buckley <calvin@cmpct.info>
2020-06-09 02:13:06 -0300

    Debug DB code

--- adventure/database.js
+++ adventure/database.js
@@ -4,7 +4,12 @@
 var pool = null;
 
 module.exports = {
+    logQueries: false,
+
     createConnection: function (params) {
+        if (params.logQueries) {
+            this.logQueries = true;
+        }
         pool = mysql.createPool(params);
     },
     
@@ -24,7 +29,9 @@ module.exports = {
             if (args.length > 2) {
                 sql_args = args[1];
             }
-            console.log(connection.format(args[0], sql_args));
+            if (this.logQueries) {
+                console.log(connection.format(args[0], sql_args));
+            }
             connection.execute(args[0], sql_args, function (err, results, fields) {
                 connection.release(); // always put connection back in pool after last query
                 if (err) {
