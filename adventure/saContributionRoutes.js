d6e04d2985d495d6cdde8b059e23325fb87974e9
nitinseshadri <61258126+nitinseshadri@users.noreply.github.com>
2020-07-25 23:10:58 -0400

    View and reject contributions

--- adventure/saContributionRoutes.js
+++ adventure/saContributionRoutes.js
@@ -1,4 +1,4 @@
-﻿var express = require("express"),
+var express = require("express"),
     fs = require("fs"),
     path = require("path"),
     middleware = require("./middleware.js"),
@@ -18,7 +18,7 @@ server.get("/sa/contributions", restrictedRoute("sa"), function (req, res) {
         var pages = Math.ceil(count / config.perPage);
         database.execute("SELECT * FROM `Contributions` WHERE `Status` = ? ORDER BY ContributionCreated DESC LIMIT ?,?", [status, (page - 1) * config.perPage, config.perPage], function (coErr, coRes, coFields) {
             var contributions = coRes.map(function (x) {
-                x.UserUUID = formatting.binToHex(x.ContributionUUID);
+                x.UserUUID = formatting.binToHex(x.UserUUID);
                 x.ContributionUUID = formatting.binToHex(x.ContributionUUID);
                 return x;
             });
@@ -31,6 +31,54 @@ server.get("/sa/contributions", restrictedRoute("sa"), function (req, res) {
         });
     });
 });
+
+server.get("/sa/contribution/:contribution", restrictedRoute("sa"), function (req, res) {
+    database.execute("SELECT * FROM `Contributions` WHERE `ContributionUUID` = ?", [formatting.hexToBin(req.params.contribution)], function (cErr, cRes, cFields) {
+        // now get any perphiery stuff
+
+        var contribution = cRes[0] || null;
+        if (cErr || contribution == null) {
+            return res.status(404).render("error", {
+                message: "There is no contribution."
+            });
+        }
+
+        var contributions = cRes.map(function (x) {
+            x.UserUUID = formatting.binToHex(x.UserUUID);
+            x.ContributionUUID = formatting.binToHex(x.ContributionUUID);
+            return x;
+        });
+
+        return res.render("saContribution", {
+            contribution: contribution,
+            platformMappingsInverted: formatting.invertObject(config.constants.platformMappings)
+        });
+
+    });
+});
+
+server.post("/sa/rejectContribution/:contribution", restrictedRoute("sa"), urlencodedParser, function (req, res) {
+    if (req.body && req.body.rejectionReason) {
+        var uuid = req.params.contribution;
+        var uuidAsBuf = formatting.hexToBin(uuid);
+        var status = "Rejected";
+        var rejectionReason = req.body.rejectionReason;
+        database.execute("UPDATE `Contributions` SET `Status` = ?, `RejectionReason` = ? WHERE `ContributionUUID` = ?", [status, rejectionReason, uuidAsBuf], function (scErr, scRes, scFields) {
+            if (scErr) {
+                return res.status(500).render("error", {
+                    message: "The contribution could not be rejected."
+                });
+            } else {
+                return res.redirect("/sa/contributions");
+            }
+        });
+    } else {
+        return res.status(400).render("error", {
+            message: "The request was malformed."
+        });
+    }
+});
+
 
 module.exports = function (c, d) {
     config = c
